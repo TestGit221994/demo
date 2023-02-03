b6f53021e99f76478e9423e1b37a94fe913d535a
Suraj Kadav <Surajk>
2023-02-03 09:53:04 +0530

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+
+    public static void main(String []args){
+
+        String str="Suraj";
+        System.out.println(str);
+
+
+
+    }
+
+
+
+
+}
